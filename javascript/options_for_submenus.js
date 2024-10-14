25af16d15ecda44335744e038f5f200fe621ca58
Jheison2023 <jmorelosmorales@gmail.com>
2024-10-14 16:09:34 -0500

    Asignación de los submenús para los menús principales

---
+++ javascript/options_for_submenus.js
@@ -0,0 +1,24 @@
+const licencias = ["Software", "Hardware", "Otros"];
+const monitor = ["Gráficas"];
+const administracion = ["Usuarios", "Permisos"];
+
+function subm_licencias() {
+    document.getElementById("nav").innerHTML = null;
+    for (let i in licencias) {
+        document.getElementById("nav").innerHTML += "<button>" +licencias[i] +"</button>";
+    }
+}
+
+function subm_monitor() {
+    document.getElementById("nav").innerHTML = null;
+    for (let i in monitor) {
+        document.getElementById("nav").innerHTML += "<button>" +monitor[i] +"</button>";
+    }
+}
+
+function subm_administracion() {
+    document.getElementById("nav").innerHTML = null;
+    for (let i in administracion) {
+        document.getElementById("nav").innerHTML += "<button>" +administracion[i] +"</button>";
+    }
+}
